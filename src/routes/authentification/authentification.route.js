fccce2343e8885ed46416f25fadc53020dec9cb7
simplyfearless1 <38662318+simplyfearless1@users.noreply.github.com>
2022-10-27 14:50:58 +0200

    Netlify Redirects, Auth page Styled Component

--- src/routes/authentification/authentification.route.js
+++ src/routes/authentification/authentification.route.js
@@ -1,15 +1,14 @@
-import "./authentification.styles"
+import { AuthentificationContainer } from "./authentification.styles";
 
-import SignUpForm from "../../components/sign-up-form/sign-up-form.component"
-import SignInForm from "../../components/sign-in-form/sign-in-form.component"
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
 const Authentification = () => {
-
-    return(
-        <div className="auth-wrap">
-            <SignInForm />
-            <SignUpForm />
-        </div>
-    )
-}
-export default Authentification
+  return (
+    <AuthentificationContainer>
+      <SignInForm />
+      <SignUpForm />
+    </AuthentificationContainer>
+  );
+};
+export default Authentification;
